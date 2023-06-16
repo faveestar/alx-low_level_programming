84dce4ca26c6ed27fd9655b819d904982bf4ed5a
Favour Abalogu <favourabalogu@gmail.com>
2023-06-16 14:32:00 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program.
+ *
+ * Return: 0 to indicate successful execution.
+ */
+int main(void)
+{
+	char lowercase;
+
+	for (lowercase = 'a'; lowercase <= 'z'; lowercase++)
+	{
+		if (lowercase != 'q' && lowercase != 'e')
+		{
+			putchar(lowercase);
+		}
+	}
+
+	putchar('\n');
+
+	return 0;
+}
