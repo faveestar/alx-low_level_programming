0b988f2d45b8f555874475fa1da7fda891ea932c
Favour Abalogu <favourabalogu@gmail.com>
2023-06-16 14:22:02 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program.
+ *
+ * Return: (0) to indicate successful execution.
+ */
+int main(void)
+{
+	char lowercase;
+	char uppercase;
+
+	for (lowercase = 'a'; lowercase <= 'z'; lowercase++)
+	{
+		putchar(lowercase);
+	}
+
+	for (uppercase = 'A'; uppercase <= 'Z'; uppercase++)
+	{
+		putchar(uppercase);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
