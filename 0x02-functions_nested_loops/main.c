353e56d87c52a86c8877eca35435767bcd71c7e9
Favour Abalogu <favourabalogu@gmail.com>
2023-06-20 16:08:36 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - Entry point of the program.
+ *
+ * Description: This program calls the print_alphabet function
+ *              to print the alphabet in lowercase.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	print_alphabet();
+
+	return (0);
+}
