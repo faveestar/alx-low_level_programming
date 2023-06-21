22ef3ce9babd4ac81e2cfe7e0910726f67cf956b
Favour Abalogu <favourabalogu@gmail.com>
2023-06-21 16:15:33 -0700

    103-fibonacci.c

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+/**
+ * sum_even_fibonacci - Calculates and prints the sum of even-valued terms in the Fibonacci sequence.
+ * @limit: The limit of the Fibonacci terms.
+ *
+ * Return: The sum of even-valued terms.
+ */
+unsigned long sum_even_fibonacci(unsigned long limit)
+{
+    unsigned long sum = 0;
+    unsigned long first = 1;
+    unsigned long second = 2;
+    unsigned long next;
+
+    while (first <= limit) {
+        if (first % 2 == 0) {
+            sum += first;
+        }
+
+        next = first + second;
+        first = second;
+        second = next;
+    }
+
+    return sum;
+}
+
+/**
+ * main - Entry point of the program.
+ *
+ * Return: 0 on successful execution.
+ */
+int main(void)
+{
+    unsigned long limit = 4000000;
+    unsigned long sum = sum_even_fibonacci(limit);
+
+    printf("The sum of even-valued terms in the Fibonacci sequence (up to %lu) is: %lu\n", limit, sum);
+
+    return 0;
+}
