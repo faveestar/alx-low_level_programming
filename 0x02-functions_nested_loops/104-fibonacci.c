19e3452bc5a0965f76cb62406d13723c986773fa
Favour Abalogu <favourabalogu@gmail.com>
2023-06-21 16:49:11 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,40 +1,49 @@
 #include <stdio.h>
 
 /**
- * print_fibonacci - Prints the first n Fibonacci numbers.
- * @n: The number of Fibonacci numbers to print.
- */
-void print_fibonacci(int n)
-{
-	int count;
-	unsigned int first = 1;
-	unsigned int second = 2;
-	unsigned int next;
-
-	printf("%u, %u", first, second);
-
-	for (count = 3; count <= n; count++)
-	{
-		next = first + second;
-		printf(", %u", next);
-
-		first = second;
-		second = next;
-	}
-
-	printf("\n");
-}
-
-/**
  * main - Entry point of the program.
  *
  * Return: 0 on successful execution.
  */
-int main(void)
-{
-	int n = 98;
 
-	print_fibonacci(n);
+	int main(void)
+	{
+		unsigned long int i;
+		unsigned long int before = 1;
+		unsigned long int after = 2;
+		unsigned long int l = 1000000000;
+		unsigned long int before1;
+		unsigned long int before2;
+		unsigned long int after1;
+		unsigned long int after2;
 
-	return (0);
-}
+
+		printf("%lu", before);
+
+
+		for (i = 1; i < 91; i++)
+		{
+			printf(", %lu", after);
+			after += before;
+			before = after - before;
+		}
+
+
+		before1 = (before / l);
+		before2 = (before % l);
+		after1 = (after / l);
+		after2 = (after % l);
+
+
+		for (i = 92; i < 99; ++i)
+		{
+			printf(", %lu", after1 + (after2 / l));
+			printf("%lu", after2 % l);
+			after1 = after1 + before1;
+			before1 = after1 - before1;
+			after2 = after2 + before2;
+			before2 = after2 - before2;
+		}
+		printf("\n");
+		return (0);
+	}
