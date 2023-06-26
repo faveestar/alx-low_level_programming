ca1d37899a324e69bc624ca9445b4747b8f4c3ef
Favour Abalogu <favourabalogu@gmail.com>
2023-06-26 09:53:09 -0700

    7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * puts_half - Prints half of a string, followed by a new line.
+ * @str: Pointer to the string.
+ *
+ * Return: None.
+ */
+void puts_half(char *str)
+{
+	int length = 0;
+	int i;
+	int start;
+
+	/* Calculate the length of the string */
+	while (str[length] != '\0')
+	{
+		length++;
+	}
+
+	/* Calculate the starting index based on string length */
+	start = (length - 1) / 2;
+	if (length % 2 != 0)
+	{
+		start++;
+	}
+
+	/* Print the second half of the string */
+	for (i = start; i < length; i++)
+	{
+		_putchar(str[i]);
+	}
+
+	_putchar('\n');
+}
